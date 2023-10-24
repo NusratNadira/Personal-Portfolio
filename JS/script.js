6dad0de1e9ea9630d4ffc70084ad513057d24946
NusratNadira <nusratnadira2@gmail.com>
2023-10-24 21:52:28 +0600

    upload

---
+++ JS/script.js
@@ -0,0 +1,39 @@
+// let navbar = document.querySelector('.navbar');
+
+// document.querySelector('#menu-icon').onclick = () =>{
+//   navbar.classList.toggle('active');
+//   loginForm.classList.remove('active');
+// }
+
+
+// function myFunction() {
+//     var x = document.getElementById("mynav");
+//     if (x.className === "navbar") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "navbar";
+//     }
+//   }
+
+  
+//     var typed=new Typed(".typing",{   
+// strings:[" ","web Designer","Web Developer","Problem Solver","Graphic Designer"],
+// typeSpeed:100,
+// backSpeed:60,
+// loop:true
+//   })
+
+var typed=new Typed(".typing",{
+  strings:["a student"," a web developer"," a graphic Designer"],
+typeSpeed:100,
+BackSpeed:60,
+loop:true
+})
+
+// var options = {
+//     strings: ['Puja', 'UI Desiner',  'Web Developer'],
+//     typeSpeed: 100,
+//     backSpeed:100,
+//     loop: true
+//   };
+// var typed = new Typed('.typing', options);
